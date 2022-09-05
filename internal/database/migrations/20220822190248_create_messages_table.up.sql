c28e869ccbdfe789387f9963dab7f6ba31974a6a
cherryReptile <lisovskiy.nikitk@gmail.com>
2022-09-05 22:04:05 +0300

    dropped unique index

--- internal/database/migrations/20220822190248_create_messages_table.up.sql
+++ internal/database/migrations/20220822190248_create_messages_table.up.sql
@@ -1,10 +1,10 @@
 CREATE TABLE messages
 (
     id         integer primary key autoincrement,
-    text       text           not null,
-    tg_id      integer unique not null,
-    user_id    integer        not null,
-    is_bot     boolean        not null,
+    text       text    not null,
+    tg_id      integer not null,
+    user_id    integer not null,
+    is_bot     boolean not null,
     command    text,
     created_at datetime,
     updated_at datetime,
